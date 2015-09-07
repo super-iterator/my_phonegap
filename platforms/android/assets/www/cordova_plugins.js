cfdb6f646570da4223082778a137306ae35a02bf
BL <super.iterator@gmail.com>
2015-09-08 01:53:21 +0200

    adding platforms: phonegap platform add ios android

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,23 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/io.gvox.plugin.phonecalltrap/www/PhoneCallTrap.js",
+        "id": "io.gvox.plugin.phonecalltrap.PhoneCallTrap",
+        "clobbers": [
+            "window.PhoneCallTrap"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.0.0",
+    "io.gvox.plugin.phonecalltrap": "0.1.0"
+}
+// BOTTOM OF METADATA
+});
